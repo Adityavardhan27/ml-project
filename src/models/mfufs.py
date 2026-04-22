357823d7aab4ace00638e34e0106e6664e0bddd9
Amangp <166824108+Amangp@users.noreply.github.com>
2026-04-22 23:43:20 +0530

    mfufs corrected

--- src/models/mfufs.py
+++ src/models/mfufs.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("Data/if_scored.csv")
+df = pd.read_csv("Data/new dataset/if_scored.csv")
 
 # -----------------------------
 # StatScore
@@ -13,32 +13,36 @@ df["StatScore"] = (
     + abs(df["GasCost_z"])
 )
 
+# Normalize StatScore
+den1 = df["StatScore"].max() - df["StatScore"].min() + 1e-9
+df["StatScore"] = (df["StatScore"] - df["StatScore"].min()) / den1
+
 # -----------------------------
 # TempScore
 # -----------------------------
-rolling_mean = df["Value"].rolling(20).mean()
+rolling_mean = df["Value_z"].rolling(20).mean().fillna(0)
 epsilon = 1e-9
 
-df["TempScore"] = abs(df["Value"] - rolling_mean) / (rolling_mean + epsilon)
+df["TempScore"] = abs(df["Value_z"] - rolling_mean) / (rolling_mean + epsilon)
 
-# Normalize
-df["StatScore"] = (df["StatScore"] - df["StatScore"].min()) / (
-    df["StatScore"].max() - df["StatScore"].min()
-)
-df["TempScore"] = (df["TempScore"] - df["TempScore"].min()) / (
-    df["TempScore"].max() - df["TempScore"].min()
-)
+# Normalize TempScore
+den2 = df["TempScore"].max() - df["TempScore"].min() + 1e-9
+df["TempScore"] = (df["TempScore"] - df["TempScore"].min()) / den2
 
 # -----------------------------
 # Final Score
 # -----------------------------
-df["FinalScore"] = 0.3 * df["IF_Score"] + 0.4 * df["StatScore"] + 0.3 * df["TempScore"]
+df["FinalScore"] = (
+    0.3 * df["IF_Score"]
+    + 0.4 * df["StatScore"]
+    + 0.3 * df["TempScore"]
+)
 
-# Threshold
+# Threshold (top 15% anomalies)
 threshold = df["FinalScore"].quantile(0.85)
-
 df["FraudFlag"] = (df["FinalScore"] > threshold).astype(int)
 
-df.to_csv("Data/final_output.csv", index=False)
+# Save
+df.to_csv("Data/new dataset/final_output.csv", index=False)
 
 print(" MF-UFS completed")
