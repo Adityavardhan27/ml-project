fede4d0b5ee1acc6c6d71b8a2626f6dd6df8a558
Amangp <166824108+Amangp@users.noreply.github.com>
2026-04-23 00:07:27 +0530

    correct labels created

--- src/evaluation/create_labels.py
+++ src/evaluation/create_labels.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 df = pd.read_csv("Data/new dataset/final_output.csv", low_memory=False)
 
-# Create label
-df["label"] = df["FraudFlag"]
 
-# Keep only useful columns (NO leakage)
-keep_cols = [
+# -----------------------------
+# FINAL CLEAN DATASET
+# -----------------------------
+df_final = df[[
     "Value_z",
     "GasCost_z",
     "GasEfficiency_z",
@@ -15,16 +15,11 @@ keep_cols = [
     "IF_Score",
     "StatScore",
     "TempScore",
-    "FinalScore",
-    "label",
-]
+    "FraudFlag"
+]]
 
-df = df[keep_cols]
+df_final.to_csv("Data/new dataset/labeled_data.csv", index=False)
 
-df.to_csv("Data/new dataset/labeled_data.csv", index=False)
-
-print(" Labels created from MF-UFS")
-print(f" Total rows    : {len(df)}")
-print(f" Normal  (0)   : {(df['label'] == 0).sum()}")
-print(f" Fraud   (1)   : {(df['label'] == 1).sum()}")
-print(f" Fraud rate    : {df['label'].mean()*100:.2f}%")
+print(" Clean labeled dataset created")
+print(f" Total rows    : {len(df_final)}")
+print(f" Fraud rate    : {df_final['FraudFlag'].mean()*100:.2f}%")
